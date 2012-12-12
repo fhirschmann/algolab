5a339e94758176ec1ee905ad69a6bd5e4cb4eb2c
Fabian Hirschmann <fabian@hirschm.net>
2012-12-13 00:18:41 +0100

    segmentation algorithm (may not be working correctly yet)

---
+++ filter/segment.py
@@ -0,0 +1,76 @@
+"""
+Segmentation algorithm for railway graphs.
+"""
+
+
+def neighbors(node):
+    return [s["id"] for s in node["successors"]]
+
+
+def walk_from(node_id, segment, col):
+    if node_id in segment:
+        return segment
+
+    unvisited = set(
+            neighbors(
+                col.find_one({"_id": node_id}))).difference(segment)
+
+    if len(unvisited) != 1:
+        return segment
+
+    segment.add(node_id)
+    visit = iter(unvisited).next()
+
+    return walk_from(visit, segment, col)
+
+
+def segment(col):
+    """
+    Segments a railway graph.
+
+    This is a very simple algorithm that extracts segments from a railway
+    graph. A segment is defined by its bounds, which is either an endpoint
+    or a switch.
+
+    For example, running `segment` on the following railway graph
+
+           A
+          / \
+         /   \
+        B----C
+       /
+      /
+     D
+
+    will yield the following segments:
+        [
+          [A, ..., B]
+          [A, ..., C]
+          [B, ..., C]
+          [D, ..., B]
+        ]
+
+    :param col: a collection cursor
+    :type col : a :class:`~pymongo.collection.Collection`
+    :returns: A list of lists (segments) of node ids
+    """
+    segments = []
+    visited = set()
+
+    for node in col.find():
+        if node["_id"] in visited:
+            continue
+
+        neighbor_ids = neighbors(node)
+        if len(neighbor_ids) != 2:
+            # this node is either an endpoint or a switch
+            for neighbor_id in neighbor_ids:
+                if neighbor_id in visited:
+                    continue
+
+                segment = walk_from(neighbor_id,
+                        set((node["_id"], )), col)
+                segments.append(segment)
+                visited.update(segment)
+
+    return segments
