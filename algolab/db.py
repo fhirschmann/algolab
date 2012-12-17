5d074fc4b8819fcafd73e2e92c1da298292f6cec
Fabian Hirschmann <fabian@hirschm.net>
2012-12-17 15:44:12 +0100

    merged create_rg and apply_reduction

--- algolab/db.py
+++ algolab/db.py
@@ -14,26 +14,6 @@ def node_for(_id, col):
     return col.find_one({"_id": _id})
 
 
-def create_node(node, neighbor_nodes=[]):
-    """
-    Creates a new dictionary, a node with its neighbors, that can be
-    inserted into a `~pymongo.collection.Collection`.
-
-    :param node: a node
-    :type node: dictionary
-    :param neighbor_nodes: list of neighbors
-    :type neighbor_nodes: list of dicts
-    """
-    return {
-            "_id": node["_id"],
-            "loc": node["loc"],
-            "successors": [{
-                "id": n["_id"],
-                "distance": node_distance(node, n)
-            } for n in neighbor_nodes]
-        }
-
-
 def node_distance(node1, node2):
     """
     Calculates the distance between two nodes in the database.
@@ -46,13 +26,7 @@ def apply_reduction(node_ids, source_col, dest_col):
     Reads all nodes identified by their `ids` from `source_col` and
     writes them to the `dest_col`.
 
-    If the source or destination node already exists, it is modified
-    to include the new neighbors.
-
-    The node ids will not be changed.
-
-    This method also calculates the distance between the newly created
-    notes in `dest_col`.
+    Please see `~algolab.db.create_rg` for details.
 
     :param node_ids: list of node ids in a segment to keep
     :type node_ids: list of integers
@@ -61,19 +35,9 @@ def apply_reduction(node_ids, source_col, dest_col):
     :param dest_col: the collection to read from
     :type dest_col: a :class:`~pymongo.collection.Collection`
     """
-    if len(node_ids) < 2:
-        raise ValueError("At least two points are required.")
-
-    for i, node_id in enumerate(node_ids):
-        neighbors = []
-
-        if i > 0:
-            neighbors.append(node_for(node_ids[i - 1], source_col))
-
-        if i < len(node_ids) - 1:
-            neighbors.append(node_for(node_ids[i + 1], source_col))
-
-        dest_col.insert(create_node(node_for(node_id, source_col), neighbors))
+    points = [(n["loc"][0], n["loc"][1], n["_id"]) for n in \
+            source_col.find({"_id": {"$in": node_ids}})]
+    return create_rg(points, dest_col)
 
 
 def create_rg(points, col):
@@ -81,6 +45,16 @@ def create_rg(points, col):
     Creates a railway graph from a given sequence of `points`
     and write it to a collection `col`.
 
+    If a node already exists, it is modified to include the new
+    neighbors.
+
+    The node ids will not be changed.
+
+    This method also calculates the distance between the newly created
+    nodes in `col`.
+
+    :param points: a curve that is approximated by a series of points
+    :type points: list of 3-tuples (x, y, id)
     :param col: a collection cursor
     :type col : a :class:`~pymongo.collection.Collection`
     """
@@ -90,15 +64,19 @@ def create_rg(points, col):
     for i, point in enumerate(points):
         neighbors = []
         if i > 0:
-            neighbors.append({"id": i - 1, "distance": edist(point[:2], points[i - 1][:2])})
+            neighbors.append({
+                "id": i - 1,
+                "distance": edist(point[:2], points[i - 1][:2])})
+
         if i < len(points) - 1:
-            neighbors.append({"id": i + 1, "distance": edist(point[:2], points[i + 1][:2])})
+            neighbors.append({
+                "id": i + 1,
+                "distance": edist(point[:2], points[i + 1][:2])})
 
         existing_node = col.find_one({"loc": point})
         if existing_node:
-            col.update(
-                    {"_id": point[2]},
-                    {"successors": existing_node["successors"] + neighbors})
+            existing_node["successors"].append(neighbors)
+            col.save(existing_node)
         else:
             col.insert({
                 "_id": point[2],
