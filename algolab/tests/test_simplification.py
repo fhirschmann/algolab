766957ea229a6f6858d80debbc1a533cc0af4a72
Fabian Hirschmann <fabian@hirschm.net>
2012-12-18 17:46:20 +0100

    merged line simp tests

--- algolab/tests/test_simplification.py
+++ algolab/tests/test_simplification.py
@@ -1,9 +1,24 @@
 import unittest2
 
+from algolab.rdp import *
 from algolab.anglered import *
 
 
-class LineTest(unittest2.TestCase):
+class RDPTest(unittest2.TestCase):
+    def test_horizontal_line(self):
+        data = [(1, 1, 1), (2, 1, 2), (4, 1, 3), (6, 1, 4)]
+        expected = [(1, 1, 1), (6, 1, 4)]
+        self.assertEqual(rdp(data, 0), expected)
+        self.assertEqual(rdp(data, 5), expected)
+
+    def test_vertical_line(self):
+        data = [(3, 1, 1), (3, 2, 2), (3, 5, 3), (3, 7, 4)]
+        expected = [(3, 1, 1), (3, 7, 4)]
+        self.assertEqual(rdp(data, 0), expected)
+        self.assertEqual(rdp(data, 5), expected)
+
+
+class AngleredTest(unittest2.TestCase):
     def test_horizontal_line(self):
         data = [(1, 1, 1), (2, 1, 2), (4, 1, 3), (6, 1, 4)]
         expected = [(1, 1, 1), (6, 1, 4)]
