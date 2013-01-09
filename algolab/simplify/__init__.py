28062732123e980ded96fc32914bbcb7f671dc6e
Fabian Hirschmann <fabian@hirschm.net>
2013-01-09 02:37:16 +0100

    documented simplify

--- algolab/simplify/__init__.py
+++ algolab/simplify/__init__.py
@@ -13,6 +13,24 @@ __all__ = ['simplify', 'rdp', 'anglereduce', 'rminner']
 
 def simplify(algo, source_col, dest_col, args=[], segmenter=ESSegmenter,
              progress=True):
+    """
+    Segments a railway graph using `segmenter`, applies the segmentation
+    algorithm `algo` with arguments `args` to `source_col` and stores the
+    result in `dest_col`.
+
+    :param algo: algorithm to use
+    :type algo: function with signature algo(points, *args)
+    :param source_col: the source collection
+    :type source_col: a :class:`~pymongo.collection.Collection`
+    :param dest_col: the destination collection
+    :type dest_col: a :class:`~pymongo.collection.Collection`
+    :param args: arguments to pass to `algo` (will be unpacked)
+    :type args: sequence
+    :param segmenter: segmenter to use
+    :type segmenter: `~algolab.segment.Segmenter`
+    :param progress: show progress
+    :type progress: boolean
+    """
     s = segmenter(source_col)
     n = s.estimated_num_segments
 
