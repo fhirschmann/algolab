55dbc1cf247fce28a5d537aa36155792e29d2c2f
Fabian Hirschmann <fabian@hirschm.net>
2013-01-09 02:32:35 +0100

    simplify: fixed segmenter choice

--- algolab/simplify/__init__.py
+++ algolab/simplify/__init__.py
@@ -13,11 +13,10 @@ __all__ = ['simplify', 'rdp', 'anglereduce', 'rminner']
 
 def simplify(algo, source_col, dest_col, args=[], segmenter=ESSegmenter,
              progress=True):
-    segmenter = ESSegmenter(source_col)
+    s = segmenter(source_col)
+    n = s.estimated_num_segments
 
-    n = segmenter.estimated_num_segments
-
-    for i, seg in enumerate(segmenter.segments_as_triplets):
+    for i, seg in enumerate(s.segments_as_triplets):
         if progress:
             sys.stdout.write("\rApplying %s to segment %i of %i" % (
                 algo.__name__, i, n))
