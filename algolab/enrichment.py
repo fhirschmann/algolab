52c2def1d13fef3772a46f8a9e55c70fb4826044
Michael Markert <markert.michael@googlemail.com>
2013-01-18 19:50:51 +0100

    Move route enrichment to station usage.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- algolab/enrichment.py
+++ algolab/enrichment.py
@@ -6,24 +6,24 @@ Railway Graph enrichment
 """
 import csv
 
-from algolab.stations import Stations
+from algolab.stations import StationUsage
 
-def enrich_with_routes(collection, station_path, routes_path):
+def enrich_with_routes(collection, station_usage_path, routes_path):
     """
     Enrich the collection by adding a routes count based on the information
     found in the station and routes files.
 
     :param collection: mongodb collection that contains a railway graph
-    :param station_path: path to the stations file
+    :param station_usge_path: path to the stations usage file
     :param routes_path: path to the routes file
     """
-    stations = Stations(station_usage_path, collection)
+    stations = StationUsage(station_usage_path, collection)
     with open(routes_path) as routes_file:
         next(routes_file)
         reader = csv.reader(routes_file, delimiter=';')
         for line in reader:
-            # TODO: await info from supervisor what unknown actually does
-            start, end, unknown = line[:3] # compensate for trailing space
+            start, end, type_ = line[:3] # compensate for trailing space
             for node in start, end:
-                collection.update({'_id': stations.get_node_id(node.strip())},
-                                  {'$inc': {'routes': 1}})
+                node = node.strip()
+                collection.update(stations.get_node_id(node),
+                                  {'$inc': {'value': stations.get_value(node)}})
